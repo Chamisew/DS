3271d0ce4005edae4d5b08b20a61bb2826428fef
Chamisew <125716628+DESHANISIRIWARDHANA@users.noreply.github.com>
2025-03-24 19:47:32 +0530

    add project register and login and authentication

---
+++ quickstart/authentication/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  # Hashes the password
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # Use email for authentication
