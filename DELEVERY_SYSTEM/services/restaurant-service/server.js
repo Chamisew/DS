add02d8f475bfd29f53e1ba72ddb8be4b332ae29
A.R.S.A.Wimalarathna <it22207418@my.sliit.lk>
2025-03-28 13:15:00 +0530

    create initial Express server setup

---
+++ DELEVERY_SYSTEM/services/restaurant-service/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Restaurant service running on port ${PORT}`);
+}); 
